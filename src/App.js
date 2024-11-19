796f094e741ef96f60c11a6e367a257d39b8518e
Lucas Hauptmann Pereira <lucass.hauptmann@gmail.com>
2024-11-19 12:33:55 -0300

    Update button component to render styling based on button type

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ function App() {
   return (
     <div>
       <div>
-        <Button primary success>Click me!</Button>
+        <Button success rounded outline>Click me!</Button>
       </div>
       <div>
-        <Button>Buy Now!</Button>
+        <Button danger outline>Buy Now!</Button>
       </div>
       <div>
-        <Button>See Deal!</Button>
+        <Button warning>See Deal!</Button>
       </div>
       <div>
-        <Button>Hide Ads</Button>
+        <Button secondary outline>Hide Ads</Button>
       </div>
       <div>
-        <Button>Something!</Button>
+        <Button primary rounded>Something!</Button>
       </div>
     </div>
   );
