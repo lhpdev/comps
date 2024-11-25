ea2aac97f992c42391d9dbb1ad16d831fe956675
Lucas Hauptmann Pereira <lucass.hauptmann@gmail.com>
2024-11-24 21:38:00 -0300

    Extract table logic into a hook and create a CounterPage

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,30 +1,13 @@
-import { useState } from 'react';
 import { GoArrowSmallDown, GoArrowSmallUp } from 'react-icons/go';
 import Table from './Table';
+import useSort from '../hooks/use-sort';
 
 function SortableTable(props) {
   const { config, data } = props;
-  const [sortOrder, setSortOrder] = useState(null);
-  const [sortBy, setSortBy] = useState(null);
-
-  const handleClick = (label) => {
-    if (sortBy && label !== sortBy) {
-      setSortOrder('asc');
-      setSortBy(label);
-      return;
-    }
-
-    if (sortOrder === null) {
-      setSortOrder('asc');
-      setSortBy(label);
-    } else if (sortOrder === 'asc') {
-      setSortOrder('desc');
-      setSortBy(label);
-    } else if (sortOrder === 'desc') {
-      setSortOrder(null);
-      setSortBy(null);
-    }
-  };
+  const { sortOrder, sortBy, sortedData, setSortColumn } = useSort(
+    data,
+    config
+  );
 
   const updatedConfig = config.map((column) => {
     if (!column.sortValue) {
@@ -34,57 +17,51 @@ function SortableTable(props) {
     return {
       ...column,
       header: () => (
-        <th className="cursor-pointer hover:bg-gray-100" onClick={() => handleClick(column.label)}>
+        <th
+          className="cursor-pointer hover:bg-gray-100"
+          onClick={() => setSortColumn(column.label)}
+        >
           <div className="flex items-center">
             {getIcons(column.label, sortBy, sortOrder)}
             {column.label}
           </div>
         </th>
-      )
+      ),
     };
-  })
+  });
 
-  let sortedData = data;
-  if (sortOrder && sortBy) {
-    const { sortValue } = config.find(column => column.label === sortBy);
-    sortedData = [...data].sort((a, b) => {
-      const valueA = sortValue(a);
-      const valueB = sortValue(b);
-
-      const reverseOrder = sortOrder === 'asc' ? 1 : -1;
-
-      if (typeof valueA === 'string') {
-        return valueA.localeCompare(valueB) * reverseOrder;
-      } else {
-        return (valueA - valueB) * reverseOrder;
-      }
-    });
-  }
-
-  return <Table {...props} data={sortedData} config={updatedConfig} />
+  return <Table {...props} data={sortedData} config={updatedConfig} />;
 }
 
 function getIcons(label, sortBy, sortOrder) {
   if (label !== sortBy) {
-    return <div>
-      <GoArrowSmallUp />
-      <GoArrowSmallDown />
-    </div>
+    return (
+      <div>
+        <GoArrowSmallUp />
+        <GoArrowSmallDown />
+      </div>
+    );
   }
 
   if (sortOrder === null) {
-    return <div>
-    <GoArrowSmallDown />
-  </div>
+    return (
+      <div>
+        <GoArrowSmallUp />
+        <GoArrowSmallDown />
+      </div>
+    );
   } else if (sortOrder === 'asc') {
-    return <div>
-      <GoArrowSmallUp />
-    </div>
+    return (
+      <div>
+        <GoArrowSmallUp />
+      </div>
+    );
   } else if (sortOrder === 'desc') {
-    return <div>
-      <GoArrowSmallUp />
-      <GoArrowSmallDown />
-    </div>
+    return (
+      <div>
+        <GoArrowSmallDown />
+      </div>
+    );
   }
 }
 
--- src/components/Table.js
+++ src/components/Table.js
@@ -6,30 +6,33 @@ function Table({ data, config, keyFn }) {
       return <Fragment key={column.label}>{column.header()}</Fragment>;
     }
 
-    return <th key={column.label}>{column.label}</th>
+    return <th key={column.label}>{column.label}</th>;
   });
 
   const renderedRows = data.map((rowData) => {
     const renderedCells = config.map((column) => {
-      return <td className="p-2" key={column.label}>{column.render(rowData)}</td>
+      return (
+        <td className="p-2" key={column.label}>
+          {column.render(rowData)}
+        </td>
+      );
     });
 
     return (
       <tr className="border-b" key={keyFn(rowData)}>
         {renderedCells}
       </tr>
-    )
-  })
+    );
+  });
+
   return (
     <table className="table-auto border-spacing-2">
       <thead>
-        <tr key="header" className="border-b-2">
-          {renderedHeaders}
-        </tr>
+        <tr className="border-b-2">{renderedHeaders}</tr>
       </thead>
       <tbody>{renderedRows}</tbody>
     </table>
-  )
-};
+  );
+}
 
 export default Table;
